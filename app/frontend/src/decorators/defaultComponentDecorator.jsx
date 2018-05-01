801a71eecc8fab2162f9aafacefa55dc5ffe4c07
brodyzt <brodyzt@gmail.com>
2018-04-30 23:41:25 -0400

    links open in new tab

--- app/frontend/src/decorators/defaultComponentDecorator.jsx
+++ app/frontend/src/decorators/defaultComponentDecorator.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 export default (decoratedHref: string, decoratedText, key: number): React.Node => {
   return (
-    <a href={decoratedHref} key={key}>
+    <a href={decoratedHref} target="_blank" key={key}>
       {decoratedText}
     </a>
   );
